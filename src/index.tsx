5993c6acf73f73bfaeff74fee9ff848bd7725d59
GurcanH <gurcanhamali@gmail.com>
2021-03-03 19:02:09 +1100

    010. State with TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom";
+import GuestList from "./state/GuestList";
 
 const App = () => {
-  return <div>Hi Gurcan!</div>;
+  return <GuestList />;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
