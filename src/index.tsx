9c61edbece79771650a262e8323209fa5452f67b
GurcanH <gurcanhamali@gmail.com>
2021-03-03 19:25:36 +1100

    014. Type Unions in State

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom";
-import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 
 const App = () => {
-  return <GuestList />;
+  return <UserSearch />;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
